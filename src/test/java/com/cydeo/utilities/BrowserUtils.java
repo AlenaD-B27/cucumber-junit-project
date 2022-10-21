8169f7842e2054b13ef8fb875ebc6b66bbd2e3ee
AlenaD-B27 <lenador.18@gmail.com>
2022-10-21 16:35:51 -0500

    Initial commit

---
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -0,0 +1,58 @@
+package com.cydeo.utilities;
+
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class BrowserUtils {
+    public static void sleep(int seconds){
+        try {
+            Thread.sleep(seconds*1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            System.out.println("Exception happened in sleep method!");
+        }
+    }
+
+    //Method info:
+    //• Name: verifyTitle()
+    //• Return type: void
+    //• Arg1: WebDriver
+    //• Arg2: String expectedTitle
+    public static void verifyTitle(String expectedTitle){
+        String actualTitle = Driver.getDriver().getTitle();
+        Assert.assertEquals(expectedTitle, actualTitle);
+    }
+
+
+
+
+    public static void waitForInvisibilityOf(WebElement element){
+        WebDriverWait wait  =new WebDriverWait(Driver.getDriver(), 10);
+        wait.until(ExpectedConditions.invisibilityOf(element));
+    }
+
+    public static void waitForInvisibilityOf(WebElement element, int seconds){
+        WebDriverWait wait  =new WebDriverWait(Driver.getDriver(), seconds);
+        wait.until(ExpectedConditions.invisibilityOf(element));
+    }
+
+    public static void waitForVisibilityOf(WebElement element){
+        WebDriverWait wait  =new WebDriverWait(Driver.getDriver(), 10);
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public static void waitForVisibilityOf(WebElement element, int seconds){
+        WebDriverWait wait  =new WebDriverWait(Driver.getDriver(), seconds);
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+
+
+
+
+
+}
---
+++ src/test/resources/features/eating_cucumber.feature
@@ -0,0 +1,7 @@
+Feature: Eating too many cucumbers may not be good for you
+  user story: Eating too much of any thing may not be good for you
+
+  Scenario: Eating a few is no problem
+    Given John is hungry
+    When he eats some cucumbers
+    Then he will be full
