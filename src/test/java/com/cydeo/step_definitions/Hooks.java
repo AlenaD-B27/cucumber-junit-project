0d149c108da472fa86e672bcdc3fea3d34df6f0a
AlenaD-B27 <lenador.18@gmail.com>
2022-10-27 11:28:03 -0500

    ScreenShot implementation is added to the Hooks class

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -3,8 +3,16 @@ package com.cydeo.step_definitions;
 import com.cydeo.utilities.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 public class Hooks {
+
+
+
+
+
    // @Before()
     public void setupScenario(){
         System.out.println("Setting up browser using cucumber @Before each scenario");
@@ -12,9 +20,13 @@ public class Hooks {
 
 
     @After
-    public void teardownScenario(){
+    public void teardownScenario(Scenario scenario){
         // We will implement taking screenshot in this method
         //System.out.println("It will be closing browser using cucumber @After each scenario");
+
+        byte [] screenshot = ((TakesScreenshot)Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+        scenario.attach(screenshot, "image/png", scenario.getName());
+
 
         Driver.closeDriver();
     }
