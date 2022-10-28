e1f5d66fc456819f7b6ed678f576c0def10ccbf3
AlenaD-B27 <lenador.18@gmail.com>
2022-10-28 10:51:49 -0500

    Adjusted Dropdowns_StepDefs.java with using the new util

--- src/test/java/com/cydeo/step_definitions/Dropdowns_StepDefs.java
+++ src/test/java/com/cydeo/step_definitions/Dropdowns_StepDefs.java
@@ -1,6 +1,7 @@
 package com.cydeo.step_definitions;
 
 import com.cydeo.pages.DropdownsPage;
+import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -22,17 +23,19 @@ public class Dropdowns_StepDefs {
     @Then("User should see below info in month dropdown")
     public void user_should_see_below_info_in_month_dropdown(List<String> expectedMonths) {
 
-        Select select = new Select(dropdownsPage.monthDropdown);
+//        Select select = new Select(dropdownsPage.monthDropdown);
+//
+//        List <WebElement> actualOptionsAsWebElement = select.getOptions();
+//
+//        List<String> actualOptionsAsString = new ArrayList<>();
+//
+//        for(WebElement each : actualOptionsAsWebElement){
+//            actualOptionsAsString.add(each.getText());
+//        }
 
-        List <WebElement> actualOptionsAsWebElement = select.getOptions();
 
-        List<String> actualOptionsAsString = new ArrayList<>();
 
-        for(WebElement each : actualOptionsAsWebElement){
-            actualOptionsAsString.add(each.getText());
-        }
-
-        Assert.assertEquals(expectedMonths, actualOptionsAsString);
+        Assert.assertEquals(expectedMonths, BrowserUtils.dropdownOptionsAsString(dropdownsPage.monthDropdown));
 
 
 
