5d2ae75aa9267c9c909c24bd314e2f6850e4650e
AlenaD-B27 <lenador.18@gmail.com>
2022-11-03 11:45:46 -0500

    Added maven report

--- src/test/java/com/cydeo/runner/CukesRunner.java
+++ src/test/java/com/cydeo/runner/CukesRunner.java
@@ -14,7 +14,8 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/cydeo/step_definitions",
         dryRun = false ,
-        tags = ""
+        tags = "@smoke",
+        publish = true
 )
 public class CukesRunner {
 }
